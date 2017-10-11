ff0dd27cc6c7930d08df775a7175b61130b1b7c2
Emanuel Caesar <emanuel.caesar@gmail.com>
2017-10-11 14:13:30 +0200

    button bro

--- CinemaSeatingSimulation/Form1.Designer.cs
+++ CinemaSeatingSimulation/Form1.Designer.cs
@@ -28,17 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Caesar Button";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormSimulation
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "FormSimulation";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
