364ec9a795161397be045cf49c621da4311a6a02
Ee Jie <eejie9@hotmail.com>
2017-10-11 14:14:13 +0200

    ejbutton

--- CinemaSeatingSimulation/Form1.Designer.cs
+++ CinemaSeatingSimulation/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 201);
+            this.button1.Location = new System.Drawing.Point(94, 191);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Ejbutton";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // FormSimulation
