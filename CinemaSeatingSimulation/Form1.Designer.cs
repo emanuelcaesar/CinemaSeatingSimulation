8a3d67c1a53825ae891a0a0b27737059198c9c18
Ee Jie <eejie9@hotmail.com>
2017-10-11 13:57:54 +0200

    jbhjhjh

--- CinemaSeatingSimulation/Form1.Designer.cs
+++ CinemaSeatingSimulation/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
@@ -38,7 +37,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(82, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
@@ -56,28 +55,20 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(223, 256);
+            this.numericUpDown1.Location = new System.Drawing.Point(233, 299);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 3;
             // 
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> b50ec4d293de1863c68f2c112a95109e52e845ba
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
             this.ClientSize = new System.Drawing.Size(581, 379);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-=======
-            this.ClientSize = new System.Drawing.Size(282, 253);
->>>>>>> b50ec4d293de1863c68f2c112a95109e52e845ba
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
