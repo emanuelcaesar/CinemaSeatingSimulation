d1298e7bd6f794c1c861fc5374263836cbe087d1
Emanuel Caesar <emanuel.caesar@gmail.com>
2017-10-11 13:55:26 +0200

    Re-Change

--- CinemaSeatingSimulation/Form1.Designer.cs
+++ CinemaSeatingSimulation/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CinemaSeatingSimulation
 {
-    partial class Form1
+    partial class FormSimulation
     {
         /// <summary>
         /// Required designer variable.
@@ -28,69 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // FormSimulation
             // 
-            this.button1.Location = new System.Drawing.Point(82, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(451, 78);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(223, 256);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 3;
-            // 
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> b50ec4d293de1863c68f2c112a95109e52e845ba
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(581, 379);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-=======
-            this.ClientSize = new System.Drawing.Size(282, 253);
->>>>>>> b50ec4d293de1863c68f2c112a95109e52e845ba
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1082, 628);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormSimulation";
+            this.Text = "FormSimulation";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-<<<<<<< HEAD
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
 =======
 >>>>>>> b50ec4d293de1863c68f2c112a95109e52e845ba
     }
--- CinemaSeatingSimulation/Form1.cs
+++ CinemaSeatingSimulation/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CinemaSeatingSimulation
 {
-    public partial class Form1 : Form
+    public partial class FormSimulation : Form
     {
-        public Form1()
+        public FormSimulation()
         {
             InitializeComponent();
         }
